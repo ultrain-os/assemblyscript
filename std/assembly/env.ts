5af4af0e544b62a05cf9eb7623fcbb1119934c07
Palmer <pengliao@live.cn>
2018-07-03 11:12:45 +0800

    remove the parameters of env.abort
    Signed-off-by: Palmer <pengliao@live.cn>

--- std/assembly/env.ts
+++ std/assembly/env.ts
@@ -1,7 +1,12 @@
 /** Environment abort function called where assertions evaluate to false / on throw. */
-declare function abort(
-  message?: string | null,
-  fileName?: string | null,
-  lineNumber?: u32,
-  columnNumber?: u32
-): void;
+
+// declare function abort(
+//   message?: string | null,
+//   fileName?: string | null,
+//   lineNumber?: u32,
+//   columnNumber?: u32
+// ): void;
+
+// remove the parameters
+declare function abort(): void;
+
